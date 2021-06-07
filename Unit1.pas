0afab2c200936ee63d18b1981aafe3f37d9e5e17
Sinfebeth <73958792+Sinfebeth@users.noreply.github.com>
2021-06-07 21:09:39 +0300

    Add files via upload

--- Unit1.pas
+++ Unit1.pas
@@ -5,7 +5,9 @@ interface
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, StdCtrls, jpeg, ExtCtrls, TeeProcs, TeEngine, Chart, Series,
-  sSkinManager;
+  sSkinManager, ComCtrls, Spin, sEdit, sSpinEdit, acDBDecimalSpinEdit,
+  acArcControls, sLabel, acDBTextFX, sGauge, sBevel, acProgressBar, sPanel,
+  Mask, sMaskEdit, sCustomComboEdit, sComboEdit;
 
 type
   TForm1 = class(TForm)
@@ -13,21 +15,19 @@ type
     graph: TChart;
     b_variable: TEdit;
     a_variable: TEdit;
-    left_border_scroll: TScrollBar;
-    right_border_scroll: TScrollBar;
     list_of_functions: TListBox;
     TLS: TLineSeries;
     lbl1: TLabel;
     lbl2: TLabel;
     lbl3: TLabel;
     lbl4: TLabel;
-    left_border_edit: TEdit;
-    right_border_edit: TEdit;
     sknmngr1: TsSkinManager;
+    lbl5: TLabel;
+    c_variable: TEdit;
+    left_border_changer: TsDecimalSpinEdit;
+    right_border_changer: TsDecimalSpinEdit;
     procedure FormCreate(Sender: TObject);
     procedure button_createClick(Sender: TObject);
-    procedure right_border_scrollChange(Sender: TObject);
-    procedure left_border_scrollChange(Sender: TObject);
   private
     { Private declarations }
   public
@@ -37,58 +37,171 @@ type
 var
   Form1: TForm1;
    h, x: real;
-   i: Integer;
-   a,b:Real;
+   i,y_mod,x_int: Integer;
+   a,b,x_arctan:Real;
 implementation
 
 {$R *.dfm}
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
+  a_variable.Text:='0';
+  b_variable.Text:='0';
+  c_variable.Text:='0';
+  
+
   with list_of_functions do
   begin
+Items.Add('y=mod(x,a)');
 Items.Add('y=x^3');
-Items.Add('f2');
-Items.Add('f3');
+Items.Add('y=a*x^3');
+Items.Add('y=cos(x)');
+Items.Add('y=1/x)');
+Items.Add('y=ln(cos(x))');
+Items.Add('y=arctng(x)');
+Items.Add('y=a*x^2+b*x+c');
+Items.Add('y=a*sin(x*b)+c');
+Items.Add('y=a*x+b');
 end;
 end;
 
 procedure TForm1.button_createClick(Sender: TObject);
 begin
      graph.SeriesList[0].Clear;
-     {левая и правая граница}
-     right_border_scroll.Max:=StrToInt(right_border_edit.text);
-     left_border_scroll.Min:=StrToInt(left_border_edit.text);
 
+//первая формула
      if list_of_functions.Selected[0] then
      begin
-  x := StrToInt(left_border_edit.text);
+  x := StrToInt(left_border_changer.text);
   h := 0.05;
+  repeat
+    {создание графика}
+    x:=x*100;
+    x_int:=trunc(x);
+    y_mod:=(x_int) mod StrToInt(a_variable.text);
+    graph.SeriesList[0].AddXY(x,y_mod);
+    x:=x/100;
+    x := x + h;
+  until x > StrToInt(right_border_changer.text) ;
+end;
 
-
+     //вторая формула
+     if list_of_functions.Selected[1] then
+     begin
+  x := StrToInt(left_border_changer.text);
+  h := 0.05;
   repeat
     {создание графика}
     graph.SeriesList[0].AddXY(x,x*x*x);
 
     x := x + h;
-  until x > StrToInt(right_border_edit.text) ;
+  until x > StrToInt(right_border_changer.text) ;
 end;
 
+      //третья формула
+     if list_of_functions.Selected[2] then
+     begin
+  x := StrToInt(left_border_changer.text);
+  h := 0.05;
+  repeat
+    {создание графика}
+    graph.SeriesList[0].AddXY(x,StrToInt(a_variable.text)*x*x*x);
 
+    x := x + h;
+  until x > StrToInt(right_border_changer.text) ;
+end;
+
+      //четвёртая формула
+     if list_of_functions.Selected[3] then
+     begin
+  x := StrToInt(left_border_changer.text);
+  h := 0.05;
+  repeat
+    {создание графика}
+    graph.SeriesList[0].AddXY(x,Cos(x));
+
+    x := x + h;
+  until x > StrToInt(right_border_changer.text) ;
+end;
+
+     //пятая формула
+     if list_of_functions.Selected[4] then
+     begin
+  x := StrToInt(left_border_changer.text);
+  h := 0.05;
+  repeat
+    {создание графика}
+    graph.SeriesList[0].AddXY(x,1/x);
+
+    x := x + h;
+  until x > StrToInt(right_border_changer.text) ;
+end;
+
+     //шестая формула
+     if list_of_functions.Selected[5] then
+     begin
+  x := StrToInt(left_border_changer.text);
+  h := 0.05;
+  repeat
+    {создание графика}
+    graph.SeriesList[0].AddXY(x,ln(Cos(x)));
+
+    x := x + h;
+  until x > StrToInt(right_border_changer.text) ;
+end;
+
+     //седьмая формула
+     if list_of_functions.Selected[6] then
+     begin
+  x := StrToInt(left_border_changer.text);
+  h := 0.05;
+  repeat
+    {создание графика}
+    x_arctan:=arctan(x);
+    graph.SeriesList[0].AddXY(x,x_arctan);
+
+    x := x + h;
+  until x > StrToInt(right_border_changer.text) ;
+end;
+
+     //восьмая формула
+     if list_of_functions.Selected[7] then
+     begin
+  x := StrToInt(left_border_changer.text);
+  h := 0.05;
+  repeat
+    {создание графика}
+    graph.SeriesList[0].AddXY(x,StrToInt(a_variable.Text)*x*x+StrToInt(b_variable.Text)*x+StrToInt(c_variable.Text));
+    x := x + h;
+  until x > StrToInt(right_border_changer.text) ;
+end;
+
+     //девятая формула
+     if list_of_functions.Selected[8] then
+     begin
+  x := StrToInt(left_border_changer.text);
+  h := 0.05;
+  repeat
+    {создание графика}
+    graph.SeriesList[0].AddXY(x,Sin(x*strtoint(b_variable.text))+strtoint(c_variable.text));
+    x := x + h;
+  until x > StrToInt(right_border_changer.text) ;
+end;
+
+ //десятая формула
+     if list_of_functions.Selected[9] then
+     begin
+  x := StrToInt(left_border_changer.text);
+  h := 0.05;
+  repeat
+    {создание графика}
+    graph.SeriesList[0].AddXY(x,strtoint(a_variable.text)*x+strtoint(b_variable.text));
+    x := x + h;
+  until x > StrToInt(right_border_changer.text) ;
+end;
   end;
-procedure TForm1.right_border_scrollChange(Sender: TObject);
-begin
-  {изменение правого предела графика}
-     right_border_edit.Text:=IntToStr(right_border_scroll.Position);
 
-       button_create.Click;
-end;
 
-procedure TForm1.left_border_scrollChange(Sender: TObject);
-begin
-      {изменение левого предела графика}
-      left_border_edit.Text:=IntToStr(left_border_scroll.Position);
-      button_create.Click;
-end;
+
 
 end.
